ee579f46084a2b5c1a64ad78820fa1239cf28ae5
Jonathan Hager <jonathan.hager@protonmail.com>
2020-05-31 19:40:15 +0200

    Initial commit

---
+++ desktop/src/com/throwgame/main/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.throwgame.main.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.throwgame.main.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
