457b5818c5cc62f44abff75b10d01e048d148f66
Jonathan Hager <jonathan.hager@protonmail.com>
2020-06-03 23:36:25 +0200

    lol

--- core/src/model/Level.java
+++ core/src/model/Level.java
@@ -28,6 +28,7 @@ public class Level {
     private boolean isReleased;
 
     private int traceIndex;
+    public boolean[] isTraceInitialised;
     public Vector2[] traces;
 
     public Level(Goal goal, Projectile projectile, int xPosPivot, int yPosPivot){
@@ -44,7 +45,8 @@ public class Level {
         this.angleSpeed = 0;
 
         traceIndex = 0;
-        traces = new Vector2[50];
+        isTraceInitialised = new boolean[50];
+        traces = new Vector2[isTraceInitialised.length];
         for(int i = 0; i < traces.length; i++){
             traces[i] = new Vector2(-10, -10);
         }
@@ -98,6 +100,10 @@ public class Level {
             traceIndex = 0;
         }
         traces[traceIndex] = newPos;
+
+        if(!isTraceInitialised[traceIndex]){
+            isTraceInitialised[traceIndex] = true;
+        }
 
         traceIndex++;
     }
